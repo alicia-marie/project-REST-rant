a10a5b3d21a59131618a859c252d233a9da3bdfb
Alicia MacEwan-Gutierrez <alicia7749@gmail.com>
2023-04-22 13:32:09 -0700

    navbar with style

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,58 @@
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+const places = [];
+
+router.get('/', (req, res) => {
+  let places = [{
+      name: 'H-Thai-ML',
+      city: 'Seattle',
+      state: 'WA',
+      cuisines: 'Thai, Pan-Asian',
+      pic: 'http://placekitten.com/250/250'
+    }, {
+      name: 'Coding Cat Cafe',
+      city: 'Phoenix',
+      state: 'AZ',
+      cuisines: 'Coffee, Bakery',
+      pic: 'http://placekitten.com/250/250'
+    }]
+  res.render('places/index', { places });
+});
+
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    //default image
+    req.body.pic = 'http://placekitten.com/400/400';
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown';
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA';
+  }
+  places.push(req.body);
+  res.redirect('/places');
+});
+
+router.get('/new', (req, res) => {
+  res.render('places/new');
+});
+
+router.use('/places', require('./controllers/places'));
+
+router.get('*', (req, res) => {
+  res.status(404).send('<h1>404 Page</h1>');
+});
+
+const port = process.env.PORT || 3000;
+router.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+module.exports = router;
+
+/*require('dotenv').config()
 const router = require('express').Router()
 
 router.get('/', (req, res) => {
@@ -48,7 +102,5 @@ router.get('*', (req, res) => {
     res.status(404).send('<h1>404 Page</h1>')
 })
 
-
-
 router.listen(process.env.PORT)
-module.exports = router
+module.exports = router*/
--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-require('dotenv').config()
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.use(express.static('public'));
+
+app.use('/places', require('./controllers/places'));
+
+app.get('/', (req, res) => {
+  res.render('Home');
+});
+
+app.get('*', (req, res) => {
+  res.render('error404');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app;
+
+
+/*require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.set('views', __dirname + '/view')
 app.set('view engine', 'jsx')
+app.set('views', __dirname + '/view')
 app.engine('jsx', require('express-react-views').createEngine())
+
 app.use(express.static('public'))
 
 app.use('/places', require('./controllers/places'))
@@ -17,5 +46,5 @@ app.get('*', (req, res) => {
   res.render('error404')
 })
 
-app.listen(process.env.PORT)
-app.use(express.urlencoded({ extended: true }))
+app.listen(process.env.PORT)*/
+//app.use(express.urlencoded({ extended: true }))
