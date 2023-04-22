d4a63d05d0e02208e847b19abdce80755057ced3
Alicia MacEwan-Gutierrez <alicia7749@gmail.com>
2023-04-22 13:39:38 -0700

    show route connected

--- views/default.jsx
+++ views/default.jsx
@@ -7,6 +7,19 @@ function Def (html) {
                 <title>Title</title>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"/>
             </head>
+            <nav>
+                <ul>
+                    <li>
+                    <a href="/">Home</a>
+                    </li>
+                    <li>
+                    <a href="/places">Places</a>
+                    </li>
+                    <li>
+                    <a href="/places/new">Add Place</a>
+                    </li>
+                </ul>
+            </nav>
             <body>
                 {html.children}
             </body>
