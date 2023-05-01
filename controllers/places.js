96a9216cedcc27e914390612f04af6d1a371151b
Alicia MacEwan-Gutierrez <alicia7749@gmail.com>
2023-05-01 16:23:11 -0700

    implementing Mongoose

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,60 @@
-require('dotenv').config()
+const router = require('express').Router()
+const db = require('../models')
+
+router.post('/', (req, res) => {
+  db.Place.create(req.body)
+  .then(() => {
+      res.redirect('/places')
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+router.post('/', (req, res) => {
+  res.send('POST /places stub')
+})
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  db.Place.findById(req.params.id)
+  .then(place => {
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET edit form stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+    res.send('GET /places/:id/rant/:rantId stub')
+})
+
+module.exports = router
+
+/*require('dotenv').config()
 const router = require('express').Router()
 
 let places = [{
@@ -92,4 +148,4 @@ router.delete('/:id', (req, res) => {
   }
 })
 
-module.exports = router
+module.exports = router*/
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,11 +2,11 @@ const React = require('react')
 const Def = require('../default')
 
 function index(data) {
-  let placesFormatted = data.places.map((place, index) => {
+let placesFormatted = data.places.map((place) => {
     return (
       <div className="col-sm-6">
         <h2>
-          <a href={`/places/${index}`} >
+          <a href={`/places/${place.id}`} >
             {place.name}
           </a>
         </h2>
@@ -19,9 +19,9 @@ function index(data) {
         </p>
       </div>
     )
-  })  
+  })
 
-  return (
+return (
     <Def>
       <main>
         <h1>PLACES INDEX PAGE</h1>
